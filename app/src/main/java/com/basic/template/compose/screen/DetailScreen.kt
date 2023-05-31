8358dba492f4e923ce5085887ef2c58f30759ab5
SBALAMU1 <balan.ssn@gmail.com>
2023-05-31 12:01:11 +0530

    Home, Detail screen

---
+++ app/src/main/java/com/basic/template/compose/screen/DetailScreen.kt
@@ -0,0 +1,39 @@
+package com.basic.template.compose.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.basic.template.compose.R
+
+@Composable
+fun DetailScreen(){
+    DetailScreenText()
+}
+
+@Composable
+fun DetailScreenText(){
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = stringResource(id = R.string.detail_screen), modifier = Modifier.padding(
+                all = dimensionResource(
+                    id = R.dimen.dp_8
+                )
+            ), fontSize = 24.sp
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    DetailScreen()
+}
