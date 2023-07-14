2158d4cd5ab6781fc3f5826eac0720f581c7a54e
SBALAMU1 <balan.ssn@gmail.com>
2023-07-14 08:28:54 +0530

    Class refactor

--- room/src/main/java/model/RoomUser.kt
+++ room/src/main/java/model/RoomUser.kt
@@ -3,51 +3,32 @@ package model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "user_table")
 data class RoomUser(
-
-    @SerializedName("id")
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0,
 
     @ColumnInfo("email")
-    @SerializedName("email")
     var userEmail: String? = null,
 
     @ColumnInfo("first_name")
-    @SerializedName("first_name")
     var userFirstName: String = "",
 
     @ColumnInfo("last_name")
-    @SerializedName("last_name")
     var userLastName: String = "",
 
     @ColumnInfo("avatar")
-    @SerializedName("avatar")
     var userAvatar: String = ""
 )
 
 
 data class UserListRoot(
-    @SerializedName("page")
-    var page: Int = 2,
-
-    @SerializedName("per_page")
-    var perPage: Int = 0,
-
-    @SerializedName("total")
-    var total: Int = 0,
-
-    @SerializedName("total_pages")
-    var totaPages: Int = 0,
-
-    @SerializedName("data")
+    @ColumnInfo("room_user_list")
     var userModelList: List<RoomUser>? = null
 )
 
-data class SingleUser(@SerializedName("data") var user: RoomUser?) {
+data class SingleUser(@ColumnInfo("room_user")var user: RoomUser?) {
     companion object
 
 }
