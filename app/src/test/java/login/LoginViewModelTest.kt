ba19f7123f477039751660bb645146b2d0867f0a
Balamurugan Selvam <balan.job@gmail.com>
2023-12-05 09:03:29 +0530

    Code refactor login

--- app/src/test/java/login/LoginViewModelTest.kt
+++ app/src/test/java/login/LoginViewModelTest.kt
@@ -2,7 +2,6 @@ package login
 
 import CoroutineTestRule
 import com.basic.template.compose.login.data.repo.LoginRepoImpl
-import com.basic.template.compose.login.domain.repo.LoginRepo
 import com.basic.template.compose.login.domain.usecases.LoginUseCases
 import com.basic.template.compose.login.ui.LoginViewModel
 import com.basic.template.network.model.NetworkResponse
@@ -15,39 +14,39 @@ class LoginViewModelTest {
 
     @get:Rule
     val coroutineTestRule = CoroutineTestRule()
-    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var viewModel: LoginViewModel
 
     @Test
     fun testLoginSuccess() {
-        val loginDataSource = FakeLoginDataSource(isApiSuccess = true)
-        val loginRepo = LoginRepoImpl(loginDataSource)
-        val loginUseCases = LoginUseCases(loginRepo)
-        loginViewModel = LoginViewModel(loginUseCases)
+        val dataSource = FakeLoginDataSource(isApiSuccess = true)
+        val repo = LoginRepoImpl(dataSource)
+        val useCases = LoginUseCases(repo)
+        viewModel = LoginViewModel(useCases)
 
         // Act
         val expectedData = TestDataUtil.getLoginSuccessResponse() as NetworkResponse.Success
 
-        loginViewModel.loginApiViewModel()
+        viewModel.loginApiViewModel()
         coroutineTestRule.testDispatcher.scheduler.runCurrent()
 
-        val actualData = loginViewModel.uiState.value as NetworkResponse.Success
+        val actualData = viewModel.uiState.value as NetworkResponse.Success
         assert(actualData.data?.token == expectedData.data?.token)
 
     }
 
     @Test
     fun testLoginFailure() {
-        val loginDataSource = FakeLoginDataSource(isApiSuccess = false)
-        val loginRepo = LoginRepoImpl(loginDataSource)
-        val loginUseCases = LoginUseCases(loginRepo)
-        loginViewModel = LoginViewModel(loginUseCases)
+        val dataSource = FakeLoginDataSource(isApiSuccess = false)
+        val repo = LoginRepoImpl(dataSource)
+        val useCases = LoginUseCases(repo)
+        viewModel = LoginViewModel(useCases)
 
         // Act
         val expectedData = TestDataUtil.getFailureResponse()
-        loginViewModel.loginApiViewModel()
+        viewModel.loginApiViewModel()
         coroutineTestRule.testDispatcher.scheduler.runCurrent()
 
-        val actualData = loginViewModel.uiState.value as NetworkResponse.Failure
+        val actualData = viewModel.uiState.value as NetworkResponse.Failure
         assert(expectedData.errorMessage == actualData.errorMessage)
     }
 
